871d5f3592c73f553a9789e416f30c7dd7c5d482
gaurav.s <gaurav.s@laptop-156.mportalindia.com>
2016-04-19 11:35:27 +0530

    added fibonacci series

--- testSample/src/testSample/printNum.java
+++ testSample/src/testSample/printNum.java
@@ -16,6 +16,23 @@ public class printNum {
 		{
 			System.out.println(ch);
 		}
+		int sum = 1;
+		
+		System.out.println("printing the fibonacci series!!");
+		
+			int n1=0,n2=1,n3,i,count=10;
+			
+			System.out.print(n1+" "+n2);
+			for(i=2;i<count;++i)
+			{
+				n3=n1+n2;
+				System.out.print(" " + n3);
+				n1=n2;
+				n2=n3;
+			}
+			
+			
+		
 	}
 
 }
