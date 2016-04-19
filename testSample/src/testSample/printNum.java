15c594e02f492183fd5a2dbe459b2c272500a01c
gaurav.s <gaurav.s@laptop-156.mportalindia.com>
2016-04-19 10:59:16 +0530

    this is test

---
+++ testSample/src/testSample/printNum.java
@@ -0,0 +1,23 @@
+package testSample;
+
+public class printNum {
+	
+	public static void main (String[] args)
+	{
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("2 * " +i + " = " + (2*i));
+			
+		}
+		
+		char ch;
+		
+		for (ch = 'A'; ch <= 'Z'; ch++)
+		{
+			System.out.println(ch);
+		}
+	}
+
+}
+
+
